c47e6c83c951fc94718d2ba723dc279e9ed33e30
Moses Huang <hhuang5@gmail.com>
2014-11-25 01:18:15 -0500

    Updated reference

--- graph.py
+++ graph.py
@@ -25,7 +25,16 @@ def includeParsing():
 def main(argv):
     # cmdLineObj = includeParsing()
     # DEFINE MAIN FUNCTIONS HERE
-    
+    data = np.loadtxt(argv[1])
+    fig = plt.figure()
+    ax = plt.gca()
+    ax.scatter(data[:,0], data[:,1])
+    #ax.set_xscale('log')
+    #ax.set_yscale('log')
+    ax.set_xlim(-100,100)
+    ax.set_ylim(-100,100)
+    plt.savefig(argv[2] + ".eps", bbox_inches='tight', format='eps', dpi=1200)
+
 
 if __name__ == '__main__':
     main(sys.argv)
